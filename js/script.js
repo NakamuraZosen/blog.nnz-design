12f421e03c3ae41a972f5f767f51587c075852b9
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2025-11-01 12:04:35 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ sidebar.insertAdjacentHTML('afterbegin', `
 //------------------------------------------------------------------------------
 async function populate() {
   const requestURL =
-    "https://zosen.nnz-design.com/page.json";
+    "https://blog.nnz-design.com/page.json";
   const request = new Request(requestURL);
 
   const response = await fetch(request);
